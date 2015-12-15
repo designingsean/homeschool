a96eba9e61efbf27cc014b2b46f0961734cc06ed
Sean Ryan <sean@designingsean.com>
2015-12-14 23:33:45 -0500

    Add method to update blanks to nils on logs model

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,12 @@ class Log < ActiveRecord::Base
   belongs_to :subject
   belongs_to :teacher
   belongs_to :book
+
+  before_save :normalize_blank_values
+
+  def normalize_blank_values
+    attributes.each do |column, value|
+      self[column].present? || self[column] = nil
+    end
+  end
 end
