e0930b76946d706f2eaec9ad06ff83e69d92835c
Sean Ryan <sean@designingsean.com>
2015-12-14 21:49:31 -0500

    Use scope to set folder instead of relative_url_root
    This solves the issue of form paths pointing to the wrong place.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # relative root
-  config.action_controller.relative_url_root = "/homeschool"
 end
