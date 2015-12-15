9ca58f75ff52cb72ef5a6f940eca001d27cbb123
Sean Ryan <sean@designingsean.com>
2015-12-14 22:06:15 -0500

    Undo all that routing mess

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # relative root
-  Homeschool::Application.config.relative_url_root = "/homeschool"
 end
