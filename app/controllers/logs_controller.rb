9911bfb02aa25c3fd2235b4eeca0b8ed18f815c4
Sean Ryan <sean@designingsean.com>
2016-01-03 22:34:03 -0500

    Change new behavior to allow adding multiple subjects

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -9,17 +9,37 @@ class LogsController < ApplicationController
   end
 
   def create
-    if logs = Log.create(log_params)
-      flash[:success] = "Log entry saved"
-    else
-      flash[:error] = "Log entry not saved"
+    #reading
+    unless log_params[:reading].blank? and log_params[:book_id].blank?
+      Log.create(:student_id => log_params[:student_id], :subject_id => 1, :teacher_id => log_params[:teacher_id], :date => log_params[:date], :description => log_params[:reading], :book_id => log_params[:book_id])
     end
+
+    #writing
+    unless log_params[:writing].blank?
+      Log.create(:student_id => log_params[:student_id], :subject_id => 2, :teacher_id => log_params[:teacher_id], :date => log_params[:date], :description => log_params[:writing])
+    end
+
+    #math
+    unless log_params[:math].blank?
+      Log.create(:student_id => log_params[:student_id], :subject_id => 3, :teacher_id => log_params[:teacher_id], :date => log_params[:date], :description => log_params[:math])
+    end
+
+    #science
+    unless log_params[:science].blank?
+      Log.create(:student_id => log_params[:student_id], :subject_id => 4, :teacher_id => log_params[:teacher_id], :date => log_params[:date], :description => log_params[:science])
+    end
+
+    #social_studies
+    unless log_params[:social_studies].blank?
+      Log.create(:student_id => log_params[:student_id], :subject_id => 5, :teacher_id => log_params[:teacher_id], :date => log_params[:date], :description => log_params[:social_studies])
+    end
+
     redirect_to controller: "logs", action: "index"
   end
 
   private
 
   def log_params
-    params.require(:log).permit(:student_id, :subject_id, :teacher_id, :date, :description, :book_id)
+    params.require(:log).permit(:student_id, :subject_id, :teacher_id, :date, :reading, :writing, :math, :science, :social_studies, :book_id)
   end
 end
