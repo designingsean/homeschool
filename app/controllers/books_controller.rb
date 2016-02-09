bd035090fb9aca0f1703604c9388f8b2736b6b97
Sean Ryan <sean@designingsean.com>
2016-02-09 16:28:29 -0500

    Add the ability to add/edit additional subject to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:author, :title)
+    params.require(:book).permit(:author, :title, :subject_id)
   end
 end
